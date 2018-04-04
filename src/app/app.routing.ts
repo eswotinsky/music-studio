df58317733818f37a4cd709dc4ef965f3ff31ac8
Eric <eric.swotinsky@gmail.com>
2018-04-04 15:26:17 -0700

    add remaining routes and navbar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,31 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PoliciesComponent } from './policies/policies.component';
+import { StudioComponent } from './studio/studio.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'studio',
+    component: StudioComponent
+  },
+  {
+    path: 'policies',
+    component: PoliciesComponent
   }
 ];
 
